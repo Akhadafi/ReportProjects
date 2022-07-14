efa0e329179a289997111b62f027778978f42fc3
Akhadafi <khadafi.dev@gmail.com>
2022-07-14 20:56:30 +0700

    Creating the Search Form

--- sales/views.py
+++ sales/views.py
@@ -1,13 +1,22 @@
 from django.shortcuts import render
 from django.views.generic import DetailView, ListView
 
+from .forms import SalesSearchForm
 from .models import Sale
 
 
 # Create your views here.
 def home_view(request):
+    form = SalesSearchForm(request.POST or None)
+
+    if request.method == "POST":
+        date_from = request.POST.get("date_from")
+        date_to = request.POST.get("date_to")
+        chart_type = request.POST.get("chart_type")
+        print(date_from, date_to, chart_type)
     context = {
         "title": "Sales",
+        "form": form,
     }
     return render(request, "sales/home.html", context)
 
