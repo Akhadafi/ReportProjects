8682fae6f1aa9709b726284b35b2854750fd5b1d
Akhadafi <khadafi.dev@gmail.com>
2022-07-18 23:17:17 +0700

    Working on the Charts

--- sales/views.py
+++ sales/views.py
@@ -4,7 +4,7 @@ from django.views.generic import DetailView, ListView
 
 from .forms import SalesSearchForm
 from .models import Sale
-from .utils import get_customer_from_id, get_salesman_from_id
+from .utils import get_chart, get_customer_from_id, get_salesman_from_id
 
 
 # Create your views here.
@@ -13,6 +13,7 @@ def home_view(request):
     positions_df = None
     merged_df = None
     df = None
+    chart = None
     form = SalesSearchForm(request.POST or None)
 
     if request.method == "POST":
@@ -54,6 +55,8 @@ def home_view(request):
 
             df = merged_df.groupby("transaction_id", as_index=False)["price"].agg("sum")
 
+            chart = get_chart(chart_type, df, labels=df["transaction_id"].values)
+
             sales_df = sales_df.to_html()
             positions_df = positions_df.to_html()
             merged_df = merged_df.to_html()
@@ -68,6 +71,7 @@ def home_view(request):
         "positions_df": positions_df,
         "merged_df": merged_df,
         "df": df,
+        "chart": chart,
     }
     return render(request, "sales/home.html", context)
 
